6c5efc4b640cc14d011584437f72de9464844c27
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-01-21 17:56:45 +0200

    Update 8. Truncate a String.js

--- 8. Truncate a String.js
+++ 8. Truncate a String.js
@@ -4,8 +4,8 @@
 
 function truncateString(str, num) {
     if(num >=str.length) return str
-    return str.slice(0, num) + '...'
-
+//     return str.slice(0, num) + '...' first solution
+       return str.substring(0, num) + '...' // second solution
   }
   
 console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
