5cb9558a2334e002a8495f1e5f4cf4c6d16e162e
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-03-01 15:11:50 +0200

    Add files via upload

---
+++ 38. Everything Be True.js
@@ -0,0 +1,31 @@
+// Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+// In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+// In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+// Remember, you can access object properties through either dot notation or [] notation.
+
+function truthCheck(collection, pre) {
+    let result = true
+    for (let i = 0; i < collection.length; i++) {
+        // console.log(collection[i][pre]);
+        if(collection[i][pre] === undefined ||
+            collection[i][pre] === 0 ||
+            collection[i][pre] === null ||
+            collection[i][pre] === '' ||
+            collection[i][pre] === false ||
+            Number.isNaN(collection[i][pre])
+            ){
+            result = false
+        }
+    }
+    return result;
+  }
+  
+//   console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
+  console.log(truthCheck([{name: "freeCodeCamp", users: [{name: "Quincy"}, {name: "Naomi"}]}, {name: "Code Radio", users: [{name: "Camperbot"}]}, {name: "", users: []}], "users"));
+  console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
+  console.log(truthCheck([{name: "Pikachu", number: 25, caught: 3}, {name: "Togepi", number: 175, caught: 1}, {name: "MissingNo", number: NaN, caught: 0}], "number"));
+
+  
